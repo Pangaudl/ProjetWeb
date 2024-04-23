a0e34cf30ae741ff40d5ed8870d5081d9721ad5d
Léry <pangaudl@3il.fr>
2024-04-23 16:58:36 +0200

    QUELQUES MODIF

--- WebPro.js
+++ WebPro.js
@@ -4,18 +4,19 @@ const url = require("url");
 const path = require('path');
 const port = 8080;
 
+app.use(express.static(path.join(__dirname,'/html')));
+app.use(express.static(path.join(__dirname,'/css')));
+//app.use(express.urlencoded({extended: true}));
 
-app.use(express.static(path.join(__dirname,'/APropos')));
-app.use(express.urlencoded({extended: true}));
-
+/********************ROUTAGE STATIC*********************/
 app.get('/index.html', function(req, res){
     console.log("Page index");
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/html/index.html');
 });
 
 app.get('/APropos.html', function(req, res){
     console.log("Page APropos");
-    res.sendFile(__dirname + '/APropos.html');
+    res.sendFile(__dirname + '/html/APropos.html');
 })
 
 app.get("/Annonces.html",function(req, res){
@@ -23,7 +24,9 @@ app.get("/Annonces.html",function(req, res){
     let login = req.query.login;
     console.log(url.parse(req.url, true) .pathname);
     console.log(login);
-    res.sendFile(__dirname + '/Annonces.html');
+    res.sendFile(__dirname + '/html/Annonces.html');
 });
-
-app.listen(8080);
+/************FIN IMPACT ROUTAGE STATIC******************/
+app.listen(port, function(req, res){
+    console.log("start !");
+});
